5e6b333e24f2a552da09e9b5960dd979d0fe9a0a
insistence <3055204202@qq.com>
2024-06-28 14:38:35 +0800

    perf: 修改代理localhost为127.0.0.1以适配部分设备解析localhost异常的问题

--- ruoyi-fastapi-frontend/vue.config.js
+++ ruoyi-fastapi-frontend/vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:9099`,
+        target: `http://127.0.0.1:9099`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
