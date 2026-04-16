83cb50e7f624eb80e952eae3e585e30f8b2dd4c1
insistence <3055204202@qq.com>
2026-04-16 15:39:21 +0800

    feat: 新增传输加密功能

--- ruoyi-fastapi-app/vue.config.js
+++ ruoyi-fastapi-app/vue.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { WeappTailwindcssDisabled } = require("./platform");
 const { UnifiedWebpackPluginV5 } = require("weapp-tailwindcss/webpack");
 
@@ -7,7 +8,43 @@ const { UnifiedWebpackPluginV5 } = require("weapp-tailwindcss/webpack");
 const config = {
   //....
   configureWebpack: {
+    resolve: {
+      alias: {
+        "node-forge/lib/util": path.resolve(
+          __dirname,
+          "src/vendor/node-forge/lib/util.js",
+        ),
+        "node-forge/lib/random": path.resolve(
+          __dirname,
+          "src/vendor/node-forge/lib/random.js",
+        ),
+        "node-forge/lib/prng": path.resolve(
+          __dirname,
+          "src/vendor/node-forge/lib/prng.js",
+        ),
+        "node-forge/lib/rsa": path.resolve(
+          __dirname,
+          "src/vendor/node-forge/lib/rsa.js",
+        ),
+      },
+    },
     plugins: [
+      new (require("webpack").NormalModuleReplacementPlugin)(
+        /node-forge[\\/]lib[\\/]util\.js$/,
+        path.resolve(__dirname, "src/vendor/node-forge/lib/util.js"),
+      ),
+      new (require("webpack").NormalModuleReplacementPlugin)(
+        /node-forge[\\/]lib[\\/]random\.js$/,
+        path.resolve(__dirname, "src/vendor/node-forge/lib/random.js"),
+      ),
+      new (require("webpack").NormalModuleReplacementPlugin)(
+        /node-forge[\\/]lib[\\/]prng\.js$/,
+        path.resolve(__dirname, "src/vendor/node-forge/lib/prng.js"),
+      ),
+      new (require("webpack").NormalModuleReplacementPlugin)(
+        /node-forge[\\/]lib[\\/]rsa\.js$/,
+        path.resolve(__dirname, "src/vendor/node-forge/lib/rsa.js"),
+      ),
       new UnifiedWebpackPluginV5({
         rem2rpx: true,
         disabled: WeappTailwindcssDisabled,
