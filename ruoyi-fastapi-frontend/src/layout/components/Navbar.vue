27a9d59270fb03a429abf8b9fcb2c4f86b7af770
insistence <3055204202@qq.com>
2026-07-29 22:26:52 +0800

    perf: 优化样式

--- ruoyi-fastapi-frontend/src/layout/components/Navbar.vue
+++ ruoyi-fastapi-frontend/src/layout/components/Navbar.vue
@@ -55,7 +55,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
-import TopNav from '@/components/TopNav'
+import TopNav from './TopNav'
 import TopBar from './TopBar'
 import Logo from './Sidebar/Logo'
 import Hamburger from '@/components/Hamburger'
